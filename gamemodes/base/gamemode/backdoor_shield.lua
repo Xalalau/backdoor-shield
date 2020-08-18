fd13c3d075922ce0b2cc3aaa8d7f9fb019c344d4
Xalalau Xubilozo <xalalau@outlook.com>
2020-08-17 22:33:10 -0300

    Started version 1.2+

--- gamemodes/base/gamemode/backdoor_shield.lua
+++ gamemodes/base/gamemode/backdoor_shield.lua
@@ -99,7 +99,7 @@ local suspect = {
 
 -- -----------------------------------------------------------------------------------
 
-local BS_VERSION = "V.1.2"
+local BS_VERSION = "V.git.1.2+"
 
 local BS_ALERT = "[Backdoor Shield]"
 local BS_BASEFOLDER = "backdoor shield/"
