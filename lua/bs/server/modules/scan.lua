ed9fa9ff94f49655fa6ca0f53637763d5fed9657
Xalalau Xubilozo <xalalau@outlook.com>
2021-03-13 03:24:06 -0300

    Decrease Scan_Folders() size by splitting the code

--- lua/bs/server/modules/scan.lua
+++ lua/bs/server/modules/scan.lua
@@ -118,29 +118,193 @@ local function JoinResults(tab, alert)
 	return resultString
 end
 
+-- Scan a folder
+local function RecursiveScan(BS, dir, results, cfgs)
+	if dir == "data/" .. BS.FOLDER.DATA then
+		return
+	end
+
+	local files, dirs = file.Find(dir.."*", "GAME")
+	
+	if not dirs or
+		dir == "addons/" .. BS.FOLDER.DATA or
+		dir == "lua/" .. BS.FOLDER.LUA  then
+
+		return
+	end
+
+	for _, fdir in pairs(dirs) do
+		if fdir ~= "/" then -- We can get a / if we start from the root
+			RecursiveScan(BS, dir .. fdir .. "/", results, cfgs)
+		end
+	end
+
+	for k,v in pairs(files) do
+		local path = dir .. v
+
+		if not cfgs.addonsFolder[path] then
+			local ext = string.GetExtensionFromFilename(v)
+			local blocked = {{}, {}}
+			local warning = {}
+			local pathAux = path
+
+			-- Filter by extension (if they are specified)
+			if extensions then
+				local isValid = false
+
+				for k,v in pairs(extensions) do
+					if ext == v then
+						isValid = true
+						break
+					end
+				end
+
+				if not isValid then return end
+			end
+
+			-- Convert the path of a file in the addons folder to a game's mounted one.
+			-- I'll save it and prevent us from scanning twice.
+			if cfgs.addonsFolderScan then
+				local correctPath = BS:Utils_ConvertAddonPath(path, true)
+
+				pathAux = correctPath
+				cfgs.addonsFolder[correctPath] = true
+			end
+
+			-- Ignore whitelisted contents
+			if CheckWhitelist(pathAux, BS.whitelistContents) then
+				return 
+			end
+
+			-- Print status
+			results.totalScanned = results.totalScanned + 1
+			if results.totalScanned == results.lastTotalPrinted + 500 then
+				print("\n" .. results.totalScanned .. " files scanned...\n")
+				results.lastTotalPrinted = results.totalScanned
+			end
+
+			-- Scanning
+			BS:Scan_String(nil, file.Read(path, "GAME"), blocked, warning)
+
+			local resultString = ""
+			local resultsList
+
+			-- Build, print and stock the result
+			if #blocked[1] > 0 or #blocked[2] > 0 or #warning > 0 then
+
+				-- Trash:
+
+				-- If it's any file with only detections from BS.suspect_suspect, discard it
+				local notImportant = 0
+
+				if (#blocked[1] + #blocked[2] == 0)  then
+					for k,v in pairs (warning) do
+						if cfgs.suspect_suspect_Aux[v] then
+							notImportant = notImportant + 1
+						end
+					end
+
+					if notImportant == #warning then
+						return
+					end
+				end
+
+				-- If it's a non Lua file with only one suspect detection or a suspect detection from
+				-- BS.suspect and other from BS.suspect_suscpec, discard it
+				if rext ~= "lua" and (#blocked[1] + #blocked[2] == 0) and (#warning == 1 or #warning == 2 and notImportant) then
+					return
+				end
+
+				-- Default risks:
+
+				-- Files inside low risk folders
+				for _,v in pairs(BS.lowRiskFolders) do
+					local start = string.find(pathAux, v, nil, true)
+					if start == 1 then
+						resultsList = results.lowRisk
+
+						break
+					end
+				end
+
+				-- Or if it's not a low risk folder
+				-- let's set a default risk to modify later
+				if not resultsList then
+					-- non Lua files are VERY unsafe
+					if ext ~= "lua" then
+						resultsList = results.highRisk
+					-- or check if it's a low risk file
+					elseif BS.lowRiskFiles[pathAux] then
+						resultsList = results.lowRisk
+					-- or set the risk based on the detection precedence
+					else
+						if #blocked[1] > 0 then resultsList = results.highRisk end
+						if not resultsList and #blocked[2] > 0 then resultsList = results.mediumRisk end
+						if not resultsList and #warning > 0 then resultsList = results.lowRisk end
+					end
+				end
+
+				-- Custom risks:
+
+				-- If we don't have a high risk but there are three or more medium risk detections, set to high risk
+				if resultsList ~= results.highRisk and #blocked[2] > 2 then
+					resultsList = results.highRisk
+				end
+
+				-- If we have a low risk but there are two or more high risk detections, set to medium risk
+				if resultsList == results.lowRisk and #blocked[1] >= 2 then
+					resultsList = results.mediumRisk
+				end
+
+				-- Build
+				resultString = path
+				resultString = resultString .. JoinResults(blocked[1], "[!!]")
+				resultString = resultString .. JoinResults(blocked[2], "[!]")
+				resultString = resultString .. JoinResults(warning, "[.]")
+				resultString = resultString .. "\n"
+
+				-- Print
+				if resultsList ~= results.lowRisk then
+					if resultsList == results.highRisk then
+						print("[[[ HIGH RISK ]]] ---------------------------------------------------------------------------- <<<")
+					end
+
+					print(resultString)
+				end
+
+				-- Stack up
+				table.insert(resultsList, resultString)
+			end
+		end
+	end 
+end
+
 -- Process the files recusively inside the aimed folders according to our white, black and suspect lists
 -- Low risk files will be reported in the logs as well, but they won't flood the console with warnings
 function BS:Scan_Folders(args, extensions)
-	local totalScanned = 0
-	local lastTotalPrinted = 0
+	-- All results
+	local results = {
+		totalScanned = 0,
+		lastTotalPrinted = 0,
+		highRisk = {},
+		mediumRisk = {},
+		lowRisk = {}
+	}
 
-	local highRisk = {}
-	local mediumRisk = {}
-	local lowRisk = {}
+	local cfgs = {
+		addonsFolder = {}, 	-- Note: results from addons folder have precedence.
+		addonsFolderScan = #args == 0 and true, -- The addons folder will not be scanned if args is set
+		suspect_suspect_Aux = {} -- Internal control
+	}
 
-	local suspect_suspect_Aux = {}
-
-	-- Results from addons folder take precedence
-	-- The addons folder will not be scanned if args are set
-	local addonsFolder = {}
-	local addonsFolderScan = #args == 0 and true
-
-	local folders = not addonsFolderScan and args or {
+	-- Select custom folders or a list of default folders
+	local folders = not cfgs.addonsFolderScan and args or {
 		"lua",
 		"gamemode",
 		"data",
 	}
 
+	-- Deal with bars
 	for k,v in pairs(folders) do
 		folders[k] = string.gsub(v, "\\", "/")
 		if string.sub(folders[k], -1) == "/" then
@@ -150,194 +314,32 @@ function BS:Scan_Folders(args, extensions)
 
 	-- Easy way to check self.suspect_suspect table (values as table indexes)
 	for _,v in pairs(self.suspect_suspect) do
-		suspect_suspect_Aux[v] = true
-	end
-
-	-- Scan a folder
-	local function ParteDir(dir)
-		if dir == "data/" .. self.FOLDER.DATA then
-			return
-		end
-
-		local files, dirs = file.Find( dir.."*", "GAME" )
-		
-		if not dirs or
-		   dir == "addons/" .. self.FOLDER.DATA or
-		   dir == "lua/" .. self.FOLDER.LUA  then
-
-			return
-		end
-
-		for _, fdir in pairs(dirs) do
-			if fdir ~= "/" then -- We can get a / if we start from the root
-				ParteDir(dir .. fdir .. "/")
-			end
-		end
-
-		for k,v in pairs(files) do
-			local path = dir .. v
-
-			if not addonsFolder[path] then
-				local ext = string.GetExtensionFromFilename(v)
-				local blocked = {{}, {}}
-				local warning = {}
-				local pathAux = path
-
-				-- Filter by extension (if they are specified)
-				if extensions then
-					local isValid = false
-
-					for k,v in pairs(extensions) do
-						if ext == v then
-							isValid = true
-							break
-						end
-					end
-
-					if not isValid then return end
-				end
-
-				-- Convert the path of a file in the addons folder to a game's mounted one.
-				-- I'll save it and prevent us from scanning twice.
-				if addonsFolderScan then
-					local correctPath = self:Utils_ConvertAddonPath(path, true)
-
-					pathAux = correctPath
-					addonsFolder[correctPath] = true
-				end
-
-				-- Ignore whitelisted contents
-				if CheckWhitelist(pathAux, self.whitelistContents) then
-					return 
-				end
-
-				-- Print status
-				totalScanned = totalScanned + 1
-				if totalScanned == lastTotalPrinted + 500 then
-					print("\n" .. totalScanned .. " files scanned...\n")
-					lastTotalPrinted = totalScanned
-				end
-
-				-- Scanning
-				self:Scan_String(nil, file.Read(path, "GAME"), blocked, warning)
-
-				local resultString = ""
-				local resultTable
-				local results
-
-				-- Build, print and stock the result
-				if #blocked[1] > 0 or #blocked[2] > 0 or #warning > 0 then
-
-					-- Trash:
-
-					-- If it's any file with only detections from self.suspect_suspect, discard it
-					local notImportant = 0
-
-					if (#blocked[1] + #blocked[2] == 0)  then
-						for k,v in pairs (warning) do
-							if suspect_suspect_Aux[v] then
-								notImportant = notImportant + 1
-							end
-						end
-
-						if notImportant == #warning then
-							return
-						end
-					end
-
-					-- If it's a non Lua file with only one suspect detection or a suspect detection from
-					-- self.suspect and other from self.suspect_suscpec, discard it
-					if rext ~= "lua" and (#blocked[1] + #blocked[2] == 0) and (#warning == 1 or #warning == 2 and notImportant) then
-						return
-					end
-
-					-- Default risks:
-
-					-- Files inside low risk folders
-					for _,v in pairs(self.lowRiskFolders) do
-						local start = string.find(pathAux, v, nil, true)
-						if start == 1 then
-							results = lowRisk
-
-							break
-						end
-					end
-
-					-- Or if it's not a low risk folder
-					-- let's set a default risk to modify later
-					if not results then
-						-- non Lua files are VERY unsafe
-						if ext ~= "lua" then
-							results = highRisk
-						-- or check if it's a low risk file
-						elseif self.lowRiskFiles[pathAux] then
-							results = lowRisk
-						-- or set the risk based on the detection precedence
-						else
-							if #blocked[1] > 0 then results = highRisk end
-							if not results and #blocked[2] > 0 then results = mediumRisk end
-							if not results and #warning > 0 then results = lowRisk end
-						end
-					end
-
-					-- Custom risks:
-
-					-- If we don't have a high risk but there are three or more medium risk detections, set to high risk
-					if results ~= highRisk and #blocked[2] > 2 then
-						results = highRisk
-					end
-
-					-- If we have a low risk but there are two or more high risk detections, set to medium risk
-					if results == lowRisk and #blocked[1] >= 2 then
-						results = mediumRisk
-					end
-
-					-- Build
-					resultString = path
-					resultString = resultString .. JoinResults(blocked[1], "[!!]")
-					resultString = resultString .. JoinResults(blocked[2], "[!]")
-					resultString = resultString .. JoinResults(warning, "[.]")
-					resultString = resultString .. "\n"
-
-					-- Print
-					if results ~= lowRisk then
-						if results == highRisk then
-							print("[[[ HIGH RISK ]]] ---------------------------------------------------------------------------- <<<")
-						end
-
-						print(resultString)
-					end
-
-					-- Stock
-					table.insert(results, resultString)
-				end
-			end
-		end 
+		cfgs.suspect_suspect_Aux[v] = true
 	end
 
 	print("\n\n -------------------------------------------------------------------")
 	print(self.ALERT .. " Scanning GMod and all the mounted contents...\n")
 
-	-- Manually installed addons have a higher chance of infection.
+	-- Manually installed addons have a much higher chance of infection.
 	-- Results from the addons folder always have the the full file paths
 	-- To avoid scanning a file twice, I record what we're doing and compare later.
-	if addonsFolderScan then
-		ParteDir("addons/")
-		addonsFolderScan = false
+	if cfgs.addonsFolderScan then
+		RecursiveScan(self, "addons/", results, cfgs)
+		cfgs.addonsFolderScan = false
 	end
 
 	-- Scan the other selected folders
 	for _,folder in pairs(folders) do
 		if folder == "" or file.Exists(folder .. "/", "GAME") then
-			ParteDir(folder == "" and folder or folder .. "/")
+			RecursiveScan(self, folder == "" and folder or folder .. "/", results, cfgs)
 		end
 	end
 
-	print("\nTotal files scanned: " .. totalScanned)
+	print("\nTotal files scanned: " .. results.totalScanned)
 
-	self:Report_Folder(highRisk, mediumRisk, lowRisk)
+	self:Report_Folder(results.highRisk, results.mediumRisk, results.lowRisk)
 
-	print("\nLow risk results: ", tostring(#lowRisk))
+	print("\nLow risk results: ", tostring(#results.lowRisk))
 	print("Check the log for more informations.\n")
 	print("------------------------------------------------------------------- \n")
 end
