2be230526cc124960e85c0d8d8458954a2731f79
Xalalau Xubilozo <xalalau@outlook.com>
2021-03-16 03:32:04 -0300

    Readd GenerateRandomName() to Utils

--- lua/bs/server/modules/utils.lua
+++ lua/bs/server/modules/utils.lua
@@ -70,3 +70,22 @@ function BS:Utils_ConvertAddonPath(path, forceConvertion)
 
     return corvertedPath or path
 end
+
+function BS:Utils_GenerateRandomName()
+    local name = string.ToTable("qwertyuiopásdfghjklç~zxcvbnm,.;´[1234567890-=!@#$%¨&*()_+QWERTYUIOPASDFGHJKLÇZXCVBNM<>")
+	local newName = ""
+	local aux, rand
+    
+	for i = 1, #name do
+		rand = math.random(#name)
+		aux = name[i]
+		name[i] = name[rand]
+		name[rand] = aux
+    end
+
+    for i = 1, #name do
+        newName = newName .. name[i]
+    end
+
+    return newName
+end
