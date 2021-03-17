044a2a12fc22fa100722c37d37b6084cc749d6d4
Xalalau Xubilozo <xalalau@outlook.com>
2021-03-17 01:32:01 -0300

    Rename Trace_Get__Aux to Trace_Get_Aux

--- lua/bs/server/modules/trace.lua
+++ lua/bs/server/modules/trace.lua
@@ -7,7 +7,7 @@
 local _debug = {}
 _debug.getinfo = debug.getinfo
 _debug.getlocal = debug.getlocal
-local function Trace_Get__Aux()
+local function Trace_Get_Aux()
 	local vars = { increment = 1, func = nil }
 	while true do
 		local func = _debug.getinfo(vars.increment, "flnSu")
@@ -23,13 +23,13 @@ end
 
 -- Try to get a stored trace given any function address
 function BS:Trace_Get()
-    local bankedTrace = self.traceBank[tostring(Trace_Get__Aux())]
+    local bankedTrace = self.traceBank[tostring(Trace_Get_Aux())]
     return bankedTrace and bankedTrace.trace or ""
 end
 
 -- Store a trace associated to a specific function that will lose it
 function BS:Trace_Set(func, name, trace)
-    local bankedTrace = self.traceBank[tostring(Trace_Get__Aux())]
+    local bankedTrace = self.traceBank[tostring(Trace_Get_Aux())]
 
     if bankedTrace then
         trace = bankedTrace.trace .. trace
