320f239e4ce679c26777e66974bf935f407385c0
Xalalau Xubilozo <xalalau@outlook.com>
2021-03-20 04:30:00 -0300

    Raname isClientLibs to isClientLib

--- lua/bs/init.lua
+++ lua/bs/init.lua
@@ -64,17 +64,17 @@ end
 
 -- Include our stuff
 
-local function includeLibs(dir, isClientLibs)
+local function includeLibs(dir, isClientLib)
     local files, dirs = file.Find( dir.."*", "LUA" )
 
     if not dirs then return end
 
     for _, fdir in pairs(dirs) do
-        includeLibs(dir .. fdir .. "/", isClientLibs)
+        includeLibs(dir .. fdir .. "/", isClientLib)
     end
 
     for k,v in pairs(files) do
-        if SERVER and isClientLibs then
+        if SERVER and isClientLib then
             AddCSLuaFile(dir .. v)
         else
             include(dir .. v)
